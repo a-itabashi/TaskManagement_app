90fb442aff9fd805058130a155b8767c77805e77
Aguru itabashi <itabashi.a@gmail.com>
2019-04-05 11:36:55 +0900

    Create seeds daete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
- validates :name, presense: true, length: {maximum: 50}
- validates :email, presense: true, length: {maximum: 255},
- validates :password, presense: true, length: {minimum: 5} 
-  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
-  uniqueness: true,
- before_validation { email.downcase! }
- has_secure_password
- has_many :tasks
+  validates :name, presence: true, length: {maximum: 50}
+  validates :email, presence: true, length: {maximum: 255},
+    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+    uniqueness: true
+  validates :password, presence: true, length: {minimum: 5}
+  before_validation { email.downcase! }
+  has_secure_password
+  has_many :tasks
 end
