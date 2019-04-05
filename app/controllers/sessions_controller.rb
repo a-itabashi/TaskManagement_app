15bebb3dfbb0f0197eeea5389605dc90fea3b925
Aguru itabashi <itabashi.a@gmail.com>
2019-04-05 13:41:34 +0900

    sessons_controllerの定義完成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "ログインしました"
+      redirect_to root_path
+    else
+      flash.now[:danger] = "もう一度入力をして下さい"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_path
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "アサインしました"
       redirect_to root_path
     else
       render 'new'
