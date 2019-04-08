d7676662c8d2ab0db9b41778cea1a1eedf3656ba
Aguru itabashi <itabashi.a@gmail.com>
2019-04-08 17:16:11 +0900

    label Modelを作成,

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_08_014529) do
+ActiveRecord::Schema.define(version: 2019_04_08_081518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "labels", force: :cascade do |t|
+    t.string "type"
+    t.bigint "task_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_labels_on_task_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
@@ -40,5 +48,6 @@ ActiveRecord::Schema.define(version: 2019_04_08_014529) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "labels", "tasks"
   add_foreign_key "tasks", "users"
 end
---
+++ spec/factories/labels.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :label do
+    type { "" }
+    task { nil }
+  end
+end
